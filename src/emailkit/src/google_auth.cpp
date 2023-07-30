c91f1b2f77885e4e2bb4e83c6e44787cd2d5fda5
Liubomyr Semkiv <ext-liubomyr.semkiv@here.com>
2023-07-31 00:43:47 +0300

    started working on simple http client

--- src/emailkit/src/google_auth.cpp
+++ src/emailkit/src/google_auth.cpp
@@ -1,9 +1,16 @@
 #include "google_auth.hpp"
 #include <fmt/format.h>
+#include <asio/ts/internet.hpp>
 #include <emailkit/http_srv.hpp>
 #include <emailkit/log.hpp>
 #include <folly/folly_uri.hpp>
 #include "uri_codec.hpp"
+
+// TODO: refactor to http client.
+#include <asio/read_until.hpp>
+#include <asio/ssl.hpp>
+#include <asio/streambuf.hpp>
+#include <asio/write.hpp>
 
 namespace emailkit {
 
@@ -24,6 +31,80 @@ bool launch_system_browser(std::string uri) {
 #endif
 }
 
+// simplest possible https client without redirecations support for google authentication.
+class https_client_t {
+   public:
+    explicit https_client_t(asio::io_context& ctx)
+        : m_ctx(ctx), m_ssl_ctx(asio::ssl::context::sslv23), m_socket(m_ctx, m_ssl_ctx) {}
+
+    void async_connect(std::string host, std::string port, async_callback<void> cb) {
+        if (m_socket.lowest_layer().is_open()) {
+            log_error("already opened");
+            cb(make_error_code(std::errc::already_connected));
+            return;
+        }
+
+        asio::ip::tcp::resolver resolver{m_ctx};
+        asio::ip::tcp::resolver::query query{host, port};
+
+        std::error_code ec;
+        auto endpoints = resolver.resolve(query, ec);
+        if (ec) {
+            log_error("resolve failed: {}", ec.message());
+            cb(ec);
+            return;
+        }
+
+        asio::async_connect(m_socket.lowest_layer(), std::move(endpoints),
+                            [cb = std::move(cb), this](std::error_code ec,
+                                                       const asio::ip::tcp::endpoint& e) mutable {
+                                if (ec) {
+                                    log_error("failed connecting: {}", ec);
+                                    return;
+                                }
+                                log_debug("connected to: {}", e.address().to_string());
+                                cb({});
+                            });
+    }
+
+    void aync_request(http_srv::request r, async_callback<http_srv::reply> cb) {
+        if (!m_socket.lowest_layer().is_open()) {
+            log_error("not opened");
+            cb(make_error_code(std::errc::not_connected));
+            return;
+        }
+
+        
+
+    }
+
+   private:
+    asio::io_context& m_ctx;
+    asio::ssl::context m_ssl_ctx;
+    asio::ssl::stream<asio::ip::tcp::socket> m_socket;
+};
+
+void async_make_http_post_request(https_client_t& client,
+                                  std::string uri_string,
+                                  async_callback<void> cb) {
+    auto uri_or_err = folly::Uri::tryFromString(uri_string);
+    if (!uri_or_err) {
+        auto err = uri_or_err.takeError();
+        std::stringstream ss;
+        ss << err;
+        log_error("failed making POST requeset, uri is invalid: {}", ss.str());
+        return;
+    }
+    auto& uri = *uri_or_err;
+
+    log_debug("uri.host(): {}", uri.host());
+
+    // using asio::ip::tcp;
+    // asio::ip::tcp::iostream s;
+    // s.expires_after(std::chrono::seconds(1));
+    // s.connect(argv[1], "http");
+}
+
 const std::string auth_page_template = R"(
 <!doctype html>
 <html lang="en"
@@ -38,7 +119,7 @@ class google_auth_t_impl : public google_auth_t,
                            public std::enable_shared_from_this<google_auth_t_impl> {
    public:
     google_auth_t_impl(asio::io_context& ctx, std::string host, std::string port)
-        : m_ctx(ctx), m_host(host), m_port(port) {}
+        : m_ctx(ctx), m_host(host), m_port(port), m_https_client(m_ctx) {}
 
     bool initialize() {
         m_srv = http_srv::make_http_srv(m_ctx, m_host, m_port);
@@ -116,8 +197,15 @@ class google_auth_t_impl : public google_auth_t,
                 token_request_params.emplace_back(
                     fmt::format("redirect_uri={}", encode_uri_component(local_site_uri("/done2"))));
                 token_request_params.emplace_back("grant_type=authorization_code");
-                const auto token_request = fmt::format("{}", fmt::join(token_request_params, "&"));
-                log_debug("token_request: {}", token_request);
+
+                const auto oauth2_token_endpoint = "https://accounts.google.com/o/oauth2/token";
+                const auto token_uri = fmt::format("{}?{}", oauth2_token_endpoint,
+                                                   fmt::join(token_request_params, "&"));
+                log_debug("token_uri: {}", token_uri);
+                // TODO: use async api here.
+                async_make_http_post_request(m_https_client, token_uri, [](std::error_code ec) {
+                    // ..
+                });
 
                 // TODO: consider having async operation here. For now we have sync one with 1s
                 // timeout.
@@ -146,6 +234,7 @@ class google_auth_t_impl : public google_auth_t,
     std::string m_host;
     std::string m_port;
     shared_ptr<http_srv::http_srv_t> m_srv;
+    https_client_t m_https_client;
 };
 }  // namespace
 
