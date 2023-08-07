7d2d12eda8025d1fa13b77c49edfa02ca98c2210
Liubomyr Semkiv <ext-liubomyr.semkiv@here.com>
2023-08-08 00:02:06 +0300

    Added TODO

--- src/emailkit/src/utils.hpp
+++ src/emailkit/src/utils.hpp
@@ -9,6 +9,8 @@ std::string escape_ctrl(const std::string& s);
 std::vector<std::string> split(const std::string& s, char delimiter);
 std::vector<std::string_view> split_views(std::string_view s, char delimiter);
 //std::vector<std::string_view> split_views(const std::string& s, char delimiter);
+
+// TODO: accept string_view.
 std::string base64_naive_decode(const std::string& s);
 std::string base64_naive_encode(const std::string& s);
 
