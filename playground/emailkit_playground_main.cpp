f837d8a16ca99caf786517d1d65988702d74b0ad
Liubomyr Semkiv <liubomyr.semkiv@ringteam.com>
2023-09-25 23:43:29 +0300

    Started working on emailkit::EmailApp

--- playground/emailkit_playground_main.cpp
+++ playground/emailkit_playground_main.cpp
@@ -17,6 +17,137 @@
 
 using namespace emailkit;
 
+namespace emailapp {
+// what is emailapp?
+// lets think about it..
+// app has its state dependent on which it presents different UI. E.g. when we have no configuration
+// we cannot do anything but need to login into the server first. now, imagine we are logged in and
+// have a connection. We should connect to the server first and then once we are connected, we can
+// do some backeground work and/or process actions from the user.
+
+// How we are supposed to organize an app that has different states? One way is to use variant
+// again: app_state = variant<Loggin_required, Working>; class Working {
+//      tree;
+//      email_list;
+//      email_preview;
+// }
+// The rendered for interface is should listen for updates from application and be ready to present
+// what the application needs.
+
+class EmailAppListener {
+   public:
+    virtual void EmailAppListener__on_statate_changed() = 0;
+};
+
+// so the app when started just checks if there is login information and starts if there is.
+// once being started, it should porbably somehow check what is current status of current cache (if
+// there is any). if there is no cache at all, the app lists some emails.
+
+class EmailApp {
+   public:
+    explicit EmailApp(asio::io_context& ctx) : m_ctx(ctx), m_imap_client() {}
+    bool initialize() {
+        m_imap_client = emailkit::imap_client::make_imap_client(m_ctx);
+        if (!m_imap_client) {
+            log_error("failed creating imap client");
+            return false;
+        }
+
+        m_google_auth = emailkit::make_google_auth(m_ctx, "127.0.0.1", "8089");
+        if (!m_google_auth) {
+            log_error("failed creating google auth");
+            return false;
+        }
+        return true;
+    }
+
+    void async_run(async_callback<void> cb) {
+        // initialize
+        async_authenticate([cb = std::move(cb)](std::error_code ec) mutable {
+            if (ec) {
+                log_error("failed to authenticate: {}", ec);
+                // TODO: what we are supposed to do in this case?
+                // I guess we should switch interface to authentication.
+                // But it would be better if we can handle specific error. What if we just don't
+                // have an internet and that is why we cannot authenticate.
+                std::abort();
+            }
+
+            cb({});
+
+            // So we authenticated, what next?
+        });
+    }
+
+    void async_authenticate(async_callback<void> cb) {
+        const auto app_creds = emailkit::google_auth_app_creds_t{
+            .client_id = "303173870696-bsun94hmoseeumiat4iaa6dr752ta805.apps.googleusercontent.com",
+            .client_secret = "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"};
+        const std::vector<std::string> scopes = {"https://mail.google.com"};
+
+        log_info("authentication in Google");
+
+        m_google_auth->async_handle_auth(
+            app_creds, scopes,
+            [this, cb = std::move(cb)](std::error_code ec, auth_data_t auth_data) mutable {
+                if (ec) {
+                    log_error("async_handle_auth failed: {}", ec);
+                    return;
+                }
+                log_info("authenticated in Google");
+
+                m_imap_client->async_connect(
+                    "imap.gmail.com", "993",
+                    [this, cb = std::move(cb), auth_data](std::error_code ec) mutable {
+                        if (ec) {
+                            log_error("failed connecting Gmail IMAP: {}", ec);
+                            return;
+                        }
+
+                        // TODO: get user email from google itself!
+                        const std::string user = "liubomyr.semkiv.test2@gmail.com";
+
+                        // now we need to authenticate on imap server
+                        m_imap_client->async_authenticate(
+                            {.user_email = user, .oauth_token = auth_data.access_token},
+                            [this, cb = std::move(cb)](
+                                std::error_code ec,
+                                emailkit::imap_client::auth_error_details_t err_details) mutable {
+                                if (ec) {
+                                    log_error("imap auth failed: {}", ec);
+                                    // TODO: how we are supposed to handle this error?
+                                    // I gues we shoud raise special error for this case and
+                                    // depdening on why auth failed either retry with same creds (if
+                                    // there is no Internet), or go to authentication. Or, we should
+                                    // somehow allow user to start over.
+                                    cb(ec);
+                                    return;
+                                }
+
+                                log_info("authenticated on IMAP server");
+                                // TODO: consider having some kind of connection info state which
+                                // would indicate information about all current connections.
+                                cb({});
+                            });
+                    });
+            });
+    }
+
+    asio::io_context& m_ctx;
+    std::shared_ptr<emailkit::imap_client::imap_client_t> m_imap_client;
+    std::shared_ptr<emailkit::google_auth_t> m_google_auth;
+};
+
+std::shared_ptr<EmailApp> make_email_app(asio::io_context& ctx, EmailAppListener& listener) {
+    auto app_instance = std::make_shared<EmailApp>(ctx);
+    if (!app_instance->initialize()) {
+        return nullptr;
+    }
+    return app_instance;
+}
+
+}  // namespace emailapp
+
 void fetch_one(imap_client::imap_client_t& client, int n) {
     namespace imap_commands = emailkit::imap_client::imap_commands;
     namespace fi = imap_commands::fetch_items;
@@ -27,14 +158,14 @@ void fetch_one(imap_client::imap_client_t& client, int n) {
                 imap_commands::fetch_items_vec_t{
                     // fi::body_t{},
                     fi::body_structure_t{},
-                    //fi::rfc822_header_t{},
+                    // fi::rfc822_header_t{},
                     // fi::envelope_t{},
                     // fi::flags_t{},
                     // fi::internal_date_t{},
-                    //fi::rfc822_t{},
-                    //fi::rfc822_header_t{},
-                    //fi::rfc822_size_t{},
-                    //fi::rfc822_text_t{},
+                    // fi::rfc822_t{},
+                    // fi::rfc822_header_t{},
+                    // fi::rfc822_size_t{},
+                    // fi::rfc822_text_t{},
 
                 }},
         [&client, n](std::error_code ec, imap_client::types::fetch_response_t r) {
@@ -217,8 +348,8 @@ void gmail_auth_test() {
                 const std::string user = "liubomyr.semkiv.test2@gmail.com";
 
                 // imap_client->async_obtain_capabilities(
-                //     [&, user, auth_data](std::error_code ec, std::vector<std::string> caps) {
-                //         if (ec) {
+                //     [&, user, auth_data](std::error_code ec, std::vector<std::string> caps) { //
+                //     if (ec) {
                 //             log_error("failed otaining capabilities: {}", ec);
                 //             return;
                 //         }
@@ -411,30 +542,36 @@ void install_handlers() {
 void install_handlers() {}
 #endif
 
+class ConsoleController : public emailapp::EmailAppListener {
+   public:
+    void EmailAppListener__on_statate_changed() override {
+        // ..
+    }
+};
+
 int main() {
-
     install_handlers();
-
     g_mime_init();
 
-    auto x = new int{};
+    asio::io_context ctx;
 
-    // int fd;
-    // if ((fd = open ("google-fetch-response.txt", O_RDONLY, 0)) == -1) {
-    // 	fprintf (stderr, "Cannot open message `%s': %s\n", "google-fetch-response.txt", g_strerror
-    // (errno)); 	return 0;
-    // }
+    ConsoleController controller;
 
-    // g_mime_init ();
-    // auto message = parse_message (fd);
-    // if (message == NULL) {
-    // 	printf ("Error parsing message\n");
-    // 	return 1;
-    // }
+    auto app = emailapp::make_email_app(ctx, controller);
+    if (!app) {
+        log_error("failed creating app");
+        return -1;
+    }
+    app->async_run([](std::error_code ec) {
+        if (ec) {
+            log_error("failed running app: {}", ec);
+            return;
+        }
 
-    gmail_auth_test();
-    // imap_parsing_test();
-    // parsing_list_flags_test();
+        log_info("application is running");
+    });
+
+    ctx.run();
 
     g_mime_shutdown();
 }
